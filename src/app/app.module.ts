7eca8ee452067f558b78a3e946169782a080eee6
Krister <krister.torsvik@gmail.com>
2017-10-10 13:28:09 +0200

    added needed packages and started the homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatMenuModule,
+  MatCardModule,
+  MatGridListModule,
+  MatTabsModule,
+  MatToolbarModule
+} from '@angular/material';
 
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    // Angular Material
+    MatButtonModule,
+    MatInputModule,
+    MatMenuModule,
+    MatCardModule,
+    MatGridListModule,
+    MatTabsModule,
+    MatToolbarModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
